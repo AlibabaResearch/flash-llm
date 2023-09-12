aa1674148929f5db102dbff0b7120751fbd9b1ce
ZHENG, Zhen <zzchman@gmail.com>
2023-09-12 17:50:39 +0800

    Update README and some copyright. (#2)

--- end2end_inference/ft_tools/huggingface_opt_convert_Phase1.py
+++ end2end_inference/ft_tools/huggingface_opt_convert_Phase1.py
@@ -1,15 +1,22 @@
-# Copyright 2023 The FLash-LLM Authors. All rights reserved.
+# Copyright (c) 2021-2022, NVIDIA CORPORATION.  All rights reserved.
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# http://www.apache.org/licenses/LICENSE-2.0
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Extended from Faster-Transformer Repo
+# This file is extended by Flash-LLM Authors based on Faster-Transformer.
+
+'''
+Convert huggingface Meta OPT model. Use https://huggingface.co/facebook/opt-125m as demo.
+'''
 
 import argparse
 import configparser
